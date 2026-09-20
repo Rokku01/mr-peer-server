d3d7b1ac40b9787712a052d6943ce2ac177adc4a
Rokku01 <132804016+Rokku01@users.noreply.github.com>
2026-09-19 23:08:08 -0400

    Create Create server.js

---
+++ Create server.js
@@ -0,0 +1,9 @@
+const { PeerServer } = require('peer');
+
+const peerServer = PeerServer({
+    port: process.env.PORT || 9000,
+    path: '/peerjs',
+    proxied: true
+});
+
+console.log('PeerJS signaling server running on /peerjs');
