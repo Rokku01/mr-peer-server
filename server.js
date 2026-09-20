da49758847b837f0a6c24728f451fa1ea4e92ce2
Rokku01 <132804016+Rokku01@users.noreply.github.com>
2026-09-19 23:44:45 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
-const { PeerServer } = require('peer');
+const express = require('express');
+const { ExpressPeerServer } = require('peer');
 
-const peerServer = PeerServer({
-    port: process.env.PORT || 9000,
-    path: '/peerjs',
-    proxied: true
+const app = express();
+const server = app.listen(process.env.PORT || 9000, () => {
+    console.log('Server listening on port ' + (process.env.PORT || 9000));
 });
 
-console.log('PeerJS on /peerjs');
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+    path: '/',
+    proxied: true,
+    allow_discovery: true
+});
+
+// Mount the PeerJS server at /peerjs
+app.use('/peerjs', peerServer);
+
+// A simple homepage so we can see it's alive
+app.get('/', (req, res) => {
+    res.send('PeerJS server is running. Try /peerjs/id');
+});
+
+// Log any errors
+process.on('uncaughtException', (err) => {
+    console.error('Uncaught error:', err);
+});
